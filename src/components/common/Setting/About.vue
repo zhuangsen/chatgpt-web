34bcd5941a0ccc61e1b175b87798bea402846670
madison <zhuangsen@live.com>
2024-02-27 12:05:45 +0800

    update

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -44,22 +44,22 @@ onMounted(() => {
       <h2 class="text-xl font-bold">
         Version - {{ pkg.version }}
       </h2>
-      <div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">
-        <p>
-          {{ $t("setting.openSource") }}
-          <a
-            class="text-blue-600 dark:text-blue-500"
-            href="https://github.com/Chanzhaoyu/chatgpt-web"
-            target="_blank"
-          >
-            GitHub
-          </a>
-          {{ $t("setting.freeMIT") }}
-        </p>
-        <p>
-          {{ $t("setting.stars") }}
-        </p>
-      </div>
+<!--      <div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">-->
+<!--        <p>-->
+<!--          {{ $t("setting.openSource") }}-->
+<!--          <a-->
+<!--            class="text-blue-600 dark:text-blue-500"-->
+<!--            href="https://github.com/Chanzhaoyu/chatgpt-web"-->
+<!--            target="_blank"-->
+<!--          >-->
+<!--            GitHub-->
+<!--          </a>-->
+<!--          {{ $t("setting.freeMIT") }}-->
+<!--        </p>-->
+<!--        <p>-->
+<!--          {{ $t("setting.stars") }}-->
+<!--        </p>-->
+<!--      </div>-->
       <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
       <p v-if="isChatGPTAPI">
         {{ $t("setting.monthlyUsage") }}：{{ config?.usage ?? '-' }}
